342e5c5d04681747f5c1577ddf153d6145460678
Rishi Vora <87199706+rishijv@users.noreply.github.com>
2024-03-03 13:12:29 -0800

    added padding to buttons

--- lib/Core/gnav_bar.dart
+++ lib/Core/gnav_bar.dart
@@ -12,27 +12,26 @@ class GoogleNavBar extends StatelessWidget {
         Get.put(BottomNavigationBarController());
 
     return Scaffold(
-        bottomNavigationBar: GNav(
-          activeColor: Colors.green,
-          color: Colors.grey,
-          backgroundColor: Colors.white,
-          tabBackgroundColor: Colors.black,
-          tabBorderRadius: 20,
-          tabMargin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          onTabChange: (value) {
-            _controller.index.value = value;
-          },
-          tabs: const [
-            GButton(icon: Icons.home, text: 'Home'),
-            GButton(icon: Icons.upload, text: 'Upload'),
-            GButton(icon: Icons.search, text: 'Discover'),
-            GButton(icon: Icons.settings, text: 'Settings'),
-            /*GButton(icon: Icons.perm_media_outlined, text: 'Home'),
-            GButton(icon: Icons.playlist_add, text: 'Upload'),
-            GButton(icon: Icons.ondemand_video_outlined, text: 'Discover'),
-            GButton(icon: Icons.settings, text: 'Settings')*/
-          ],
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GNav(
+            activeColor: Colors.green,
+            color: Colors.grey,
+            backgroundColor: Colors.white,
+            tabBackgroundColor: Colors.black,
+            tabBorderRadius: 20,
+            tabMargin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            onTabChange: (value) {
+              _controller.index.value = value;
+            },
+            tabs: const [
+              GButton(icon: Icons.home, text: 'Home'),
+              GButton(icon: Icons.upload, text: 'Upload'),
+              GButton(icon: Icons.search, text: 'Discover'),
+              GButton(icon: Icons.settings, text: 'Settings'),
+            ],
+          ),
         ),
         body: Obx(
           () => _controller.pages[_controller.index.value],
