8b1b097d1355368ff9686aa7abcda7ae78a3c454
Dhruv A <dhruvarora202520@gmail.com>
2024-03-03 10:49:30 -0800

    Fixed the top of the home_page

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -7,9 +7,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container (
-          padding: const EdgeInsets.fromLTRB(40, 150, 40, 150),
+      body: Container (
           width: MediaQuery.of(context).size.width,
           decoration: const BoxDecoration(
             gradient: LinearGradient(
@@ -21,9 +19,36 @@ class HomePage extends StatelessWidget {
               ],
             ),
           ),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 45),
+            alignment: Alignment.topCenter,
+            child: Column(children: [
+              const Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Papyrus", style: TextStyle(fontSize: 40, color: Colors.white, fontWeight: FontWeight.bold, fontFamily: "Avenir Next LT Pro"),),
+                  Image(image: AssetImage('assets/transparent_logo.png'), width: 80, height: 80),
+                ],
+              ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 0),
+              height: 4,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    spreadRadius: 1,
+                    blurRadius: 2,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+            ),
+          ],
+          ),
+          ),
           
       ),
-    ),
     );
   }
 }
